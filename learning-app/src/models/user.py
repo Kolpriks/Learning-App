9f9e686819ac19615d1dffa37643b6cee6b0e29f
RayzorST <mihailcherry@vk.com>
2024-03-09 22:50:36 +0300

    id, name, surname, age

--- learning-app/src/models/user.py
+++ learning-app/src/models/user.py
@@ -1,6 +1,12 @@
-from peewee import Model
+from peewee import *
+import configs.database
 
 class UserModel(Model):
     class Meta:
-        datatable = None
+        datatable = configs.database.db
         db_table = 'Users'
+
+    id = PrimaryKeyField()
+    name = CharField(max_length=20)
+    surname = CharField(max_length=20)
+    age = IntegerField()
