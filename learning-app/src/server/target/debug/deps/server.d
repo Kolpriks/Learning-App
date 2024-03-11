9185274e2a789304bbd6a2ee7adfe3eaa0834a0a
Patosya <149209415+Pat0sya@users.noreply.github.com>
2024-03-12 00:47:40 +0300

    bugfix with main

---
+++ learning-app/src/server/target/debug/deps/server.d
@@ -0,0 +1,5 @@
+C:\Users\Patos'ka\code\learning-app\learning-app\src\server\target\debug\deps\server.exe: src\main.rs
+
+C:\Users\Patos'ka\code\learning-app\learning-app\src\server\target\debug\deps\server.d: src\main.rs
+
+src\main.rs:
