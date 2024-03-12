7e5c574532a1be7d685122227166f14df6068e5d
Patosya <149209415+Pat0sya@users.noreply.github.com>
2024-03-13 00:43:39 +0300

    Rust with HTTP support OUT NOW!

--- learning-app/src/server/src/main.rs
+++ learning-app/src/server/src/main.rs
@@ -1,21 +1,35 @@
+use std::fs;
 use std::net::{TcpListener, TcpStream};
 use std::io::prelude::*;
 
 fn main() {
     //TODO Ввести адрес сервера
     let listner =
+
         TcpListener::bind("127.0.0.1:7878").unwrap();
+
     for stream in listner.incoming(){
+
         let stream = stream.unwrap();
+
         handle_connection(stream);
     }
 }
 fn handle_connection(mut stream: TcpStream){
+
     let mut buffer: [u8; 1024] = [0; 1024];
 
     stream.read(&mut buffer).unwrap();
-    println!(
-        "Запрос: {}",
-        String::from_utf8_lossy(&buffer[..])
-    )
+    //Сделал полноценную HTTP поддержку
+    //Только надо теперь ввести верный индекс
+    let contents: String = fs::read_to_string("index.html").unwrap();
+        let response: String = format!(
+        "HTTP/1.1 200 OK\r\nContent-length: {}\r\n\r\n{}",  
+        contents.len(),                                           
+        contents
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+
+    stream.flush().unwrap();
 }
