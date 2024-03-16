c97bf88317eba8702d43e0a29772ccfc16e416dd
Kolpriks <vladkulikov348@gmail.com>
2024-03-16 16:36:38 +0300

    Connet established

--- target/debug/deps/server.d
+++
@@ -1,5 +0,0 @@
-C:\All for Kolpriks\Study Folder\Mobiles-Project\Learning-App\target\debug\deps\server.exe: learning-app/src/server/src/main.rs
-
-C:\All for Kolpriks\Study Folder\Mobiles-Project\Learning-App\target\debug\deps\server.d: learning-app/src/server/src/main.rs
-
-learning-app/src/server/src/main.rs:
